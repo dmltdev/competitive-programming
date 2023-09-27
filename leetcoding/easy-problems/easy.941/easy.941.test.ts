bde917d5973a1b2fa9e45d4cec3299fb860e9a03
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-09-27 08:56:43 +0300

    feat: adding Jest tests, easy 941 ts

---
+++ leetcoding/easy-problems/easy.941/easy.941.test.ts
@@ -0,0 +1,13 @@
+import validMountainArray from "./easy.941";
+
+describe("Valid Mountain Array", () => {
+  it("Negative cases", () => {
+    expect(validMountainArray([3, 5, 5])).toBe(false);
+    expect(validMountainArray([3, 2, 1])).toBe(false);
+  })
+
+  it("Positive cases", () => {
+    expect(validMountainArray([0, 1, 2, 3, 4, 3, 2, 1, 0])).toBe(true);
+    expect(validMountainArray([3, 5, 2])).toBe(true);
+  })
+})
