3972a0475451dd3e81888ca202b32c1ec755b911
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-09-27 08:57:27 +0300

    tests: easy.941.test.ts

--- leetcoding/easy-problems/easy.941/easy.941.test.ts
+++ leetcoding/easy-problems/easy.941/easy.941.test.ts
@@ -4,10 +4,11 @@ describe("Valid Mountain Array", () => {
   it("Negative cases", () => {
     expect(validMountainArray([3, 5, 5])).toBe(false);
     expect(validMountainArray([3, 2, 1])).toBe(false);
-  })
+    expect(validMountainArray([])).toBe(false);
+  });
 
   it("Positive cases", () => {
     expect(validMountainArray([0, 1, 2, 3, 4, 3, 2, 1, 0])).toBe(true);
     expect(validMountainArray([3, 5, 2])).toBe(true);
-  })
-})
+  });
+});
