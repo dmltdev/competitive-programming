158dd61166182a4a2d05629de9450a5e5630b375
dmltdev <hone.domitoro@gmail.com>
2023-11-21 18:50:05 +0200

    feat: medium 2705 ts

--- leetcoding/medium-problems/medium.2705/medium.2705.ts
+++ leetcoding/medium-problems/medium.2705/medium.2705.ts
@@ -31,6 +31,39 @@ obj is a valid JSON object
 
 */
 
-const compactObject = function (obj) {
-  
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | Array<JSONValue>;
+
+const compactObject = (obj: Obj): Obj => {
+  function dfs(obj: any): any {
+    if (!obj) return false;
+    if (typeof obj !== 'object') return obj;
+
+    if (Array.isArray(obj)) {
+      const newArr: any[] = [];
+      for (let i = 0; i < obj.length; i++) {
+        const curr = obj[i];
+        const subRes = dfs(curr);
+
+        if (subRes) {
+          newArr.push(subRes);
+        }
+      }
+
+      return newArr;
+    }
+
+    const newObj: Obj = {};
+
+    for (const key in obj) {
+      const subRes = dfs(obj[key]);
+      if (subRes) {
+        newObj[key] = subRes;
+      }
+    }
+
+    return newObj;
+  }
+
+  return dfs(obj);
 };
