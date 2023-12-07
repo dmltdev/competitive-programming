f8e2ace24cc101d6090141ce364d1ef7deed9c3d
dmltdev <hone.domitoro@gmail.com>
2023-12-07 12:19:15 +0200

    feat: easy 1903

---
+++ leetcoding/easy-problems/easy.1903/easy.1903.ts
@@ -0,0 +1,68 @@
+/* 
+1903. Largest Odd Number in String
+
+You are given a string num, representing a large integer. Return the largest-valued odd integer (as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
+
+A substring is a contiguous sequence of characters within a string.
+
+ 
+
+Example 1:
+
+Input: num = "52"
+Output: "5"
+Explanation: The only non-empty substrings are "5", "2", and "52". "5" is the only odd number.
+Example 2:
+
+Input: num = "4206"
+Output: ""
+Explanation: There are no odd numbers in "4206".
+Example 3:
+
+Input: num = "35427"
+Output: "35427"
+Explanation: "35427" is already an odd number.
+ 
+
+Constraints:
+
+1 <= num.length <= 105
+num only consists of digits and does not contain any leading zeros.
+*/
+
+/* //! This solution does not work with extremely large numbers
+function largestOddNumber(num: string): string {
+  if (Number(num) % 2 === 1) {
+    return num;
+  }
+
+  const substrings: string[] = [];
+  for (let i = 0; i < num.length; i++) {
+    for (let j = i + 1; j <= num.length; j++) {
+      substrings.push(num.slice(i, j));
+    }
+  }
+
+  const oddNumbers = substrings.filter(x => Number(x) % 2 !== 0);
+
+  if (oddNumbers.length === 0) {
+    return '';
+  }
+
+  return Math.max(...oddNumbers.map(Number)).toString();
+}
+
+largestOddNumber('239537672423884969653287101');
+*/
+
+function largestOddNumber(num: string): string {
+  for (let i = num.length - 1; i >= 0; i--) {
+    if (parseInt(num[i]) % 2 === 1) {
+      return num.slice(0, i + 1);
+    }
+  }
+
+  return '';
+}
+
+largestOddNumber('239537672423884969653287101'); //?
