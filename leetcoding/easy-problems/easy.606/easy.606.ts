3cfe297c313021cd87111d79db2b0d77f3d128c2
dmltdev <hone.domitoro@gmail.com>
2023-12-11 13:27:39 +0200

    feat: easy 606 ts & py

---
+++ leetcoding/easy-problems/easy.606/easy.606.ts
@@ -0,0 +1,62 @@
+/* 
+606. Construct String from Binary Tree
+
+Given the root of a binary tree, construct a string consisting of parenthesis and integers from a binary tree with the preorder traversal way, and return it.
+
+Omit all the empty parenthesis pairs that do not affect the one-to-one mapping relationship between the string and the original binary tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4]
+Output: "1(2(4))(3)"
+Explanation: Originally, it needs to be "1(2(4)())(3()())", but you need to omit all the unnecessary empty parenthesis pairs. And it will be "1(2(4))(3)"
+Example 2:
+
+
+Input: root = [1,2,3,null,4]
+Output: "1(2()(4))(3)"
+Explanation: Almost the same as the first example, except we cannot omit the first parenthesis pair to break the one-to-one mapping relationship between the input and the output.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-1000 <= Node.val <= 1000
+*/
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function tree2str(root: TreeNode | null): string {
+  const result: string[] = [];
+
+  function preorder(root: TreeNode | null) {
+    if (root === null) return;
+
+    result.push('(');
+    result.push(root.val.toString());
+
+    if (!root.left && root.right) {
+      result.push('()');
+    }
+    
+    preorder(root.left);
+    preorder(root.right);
+
+    result.push(')');
+  }
+
+  preorder(root);
+  return result.join('').slice(1, -1);
+}
