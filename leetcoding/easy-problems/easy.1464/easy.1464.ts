f7ad381d1966c3ad2bb6a56b63c3c707dff85db7
dmltdev <hone.domitoro@gmail.com>
2023-12-12 10:11:37 +0200

    feat(1464): new solution with ts and py
    - add a better algorithm for typescript
    - add the same algorithm in python

--- leetcoding/easy-problems/easy.1464/easy.1464.ts
+++ leetcoding/easy-problems/easy.1464/easy.1464.ts
@@ -36,18 +36,19 @@ function maxProduct(nums: number[]): number {
 }
 */
 
-function maxProduct (nums) {
-  let max1 = -Infinity;
-  let max2 = -Infinity;
+//! Space O(1), time O(n)
+function maxProduct(nums: number[]): number {
+  let max1: number = -Infinity;
+  let max2: number = -Infinity;
 
   for (const num of nums) {
-      if (num >= max1) {
-          max2 = max1;
-          max1 = num;
-      } else if (num > max2) {
-          max2 = num;
-      }
+    if (num >= max1) {
+      max2 = max1;
+      max1 = num;
+    } else if (num > max2) {
+      max2 = num;
+    }
   }
 
   return (max1 - 1) * (max2 - 1);
-};
+}
