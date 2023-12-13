2fba03fabf84cab12b7abfefac83de6773062382
dmltdev <hone.domitoro@gmail.com>
2023-12-13 09:40:17 +0200

    test(coverage): add easy.1582 solution with tests

---
+++ leetcoding/easy-problems/easy.1582/easy.1582.test.ts
@@ -0,0 +1,38 @@
+import numSpecial from './easy.1582.ts';
+
+describe('numSpecial [easy, 1582]', () => {
+  test('it should return the correct number of special positions in a binary matrix', () => {
+    const matrix1: number[][] = [
+      [1, 0, 0],
+      [0, 0, 1],
+      [1, 0, 0],
+    ];
+    expect(numSpecial(matrix1)).toEqual(1);
+
+    const matrix2: number[][] = [
+      [1, 0, 0],
+      [0, 1, 0],
+      [0, 0, 1],
+    ];
+    expect(numSpecial(matrix2)).toEqual(3);
+
+    const matrix3: number[][] = [
+      [0, 0, 0, 1],
+      [1, 0, 0, 0],
+      [0, 1, 1, 0],
+      [0, 0, 0, 0],
+    ];
+    expect(numSpecial(matrix3)).toEqual(2);
+  });
+
+  test("it should return 0 when there are no special positions in a matrix", () => {
+    const matrix: number[][] = [
+      [1, 1, 1, 1],
+      [1, 1, 1, 1],
+      [1, 1, 1, 1],
+      [1, 1, 1, 1],
+    ];
+
+    expect(numSpecial(matrix)).toEqual(0);
+  })
+});
