e7d97ac9dd07227d05f09d96feeb5be4e3b904d0
dmltdev <hone.domitoro@gmail.com>
2024-01-18 08:53:04 +0200

    feat(70): climbing stairs

---
+++ leetcoding/easy-problems/easy.70/easy.70.ts
@@ -0,0 +1,46 @@
+/* 
+70. Climbing Stairs
+
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ 
+
+Constraints:
+
+1 <= n <= 45
+*/
+
+function climbStairs(n: number): number {
+  if (n <= 2) return n;
+  let steps = 0;
+  let prevShortStep = 1,
+    prevLongStep = 2;
+
+  for (let i = 1; i < n - 1; i++) {
+    steps = prevShortStep + prevLongStep;
+    [prevShortStep, prevLongStep] = [prevLongStep, steps];
+  }
+
+  return steps;
+}
+
+climbStairs(5) //?
