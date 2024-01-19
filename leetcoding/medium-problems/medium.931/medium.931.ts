071409fbc27703cf5d8e6adc427dc6c4915aab28
dmltdev <hone.domitoro@gmail.com>
2024-01-19 08:45:07 +0200

    feat(931): minimum falling path sum

---
+++ leetcoding/medium-problems/medium.931/medium.931.ts
@@ -0,0 +1,54 @@
+/* 
+931. Minimum Falling Path Sum
+
+Given an n x n array of integers matrix, return the minimum sum of any falling path through matrix.
+
+A falling path starts at any element in the first row and chooses the element in the next row that is either directly below or diagonally left/right. Specifically, the next element from position (row, col) will be (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1).
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[2,1,3],[6,5,4],[7,8,9]]
+Output: 13
+Explanation: There are two falling paths with a minimum sum as shown.
+Example 2:
+
+
+Input: matrix = [[-19,57],[-40,-5]]
+Output: -59
+Explanation: The falling path with a minimum sum is shown.
+ 
+
+Constraints:
+
+n == matrix.length == matrix[i].length
+1 <= n <= 100
+-100 <= matrix[i][j] <= 100
+
+*/
+
+function minFallingPathSum(matrix: number[][]): number {
+  let prevRow: number[] = [...matrix[0]];
+
+  for (let i = 1; i < matrix.length; i++) {
+    const currRow: number[] = new Array(matrix.length).fill(0);
+    for (let j = 0; j < matrix[0].length; j++) {
+      const curr: number = matrix[i][j];
+      const top: number = curr + prevRow[j];
+      const topLeft: number = curr + (prevRow[j - 1] ?? Infinity);
+      const topRight: number = curr + (prevRow[j + 1] ?? Infinity);
+      currRow[j] = Math.min(top, topLeft, topRight);
+    }
+    prevRow = [...currRow];
+  }
+
+  return Math.min(...prevRow);
+}
+
+minFallingPathSum([
+  [2, 1, 3],
+  [6, 5, 4],
+  [7, 8, 9],
+]); //?
