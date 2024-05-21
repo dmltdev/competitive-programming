c4f6bf721add84dcd73f6173aed96e545afdc1e8
dmltdev <hone.domitoro@gmail.com>
2024-05-21 14:28:43 +0300

    feat(78): subsets

--- leetcoding/medium-problems/medium.102/medium.102.ts
+++ leetcoding/medium-problems/medium.102/medium.102.ts
@@ -26,7 +26,7 @@ The number of nodes in the tree is in the range [0, 2000].
 -1000 <= Node.val <= 1000
 */
 
-class TreeNode {
+export class TreeNode {
   val: number;
   left: TreeNode | null;
   right: TreeNode | null;
