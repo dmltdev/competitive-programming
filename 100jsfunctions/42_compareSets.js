9ced7dfddb8629080a9a247184cc1dcca84d0092
dmltdev <hone.domitoro@gmail.com>
2024-06-11 12:26:10 +0300

    feat(100jsfunctions): 42 compareSets

---
+++ 100jsfunctions/42_compareSets.js
@@ -0,0 +1,62 @@
+/*
+42.compareSets
+Write a function named compareSets that receives 2 parameters.
+
+a Set of numbers - setA
+a Set of numbers - setB
+The function should compare the 2 sets and return an Object with the following properties:
+
+onlyA - a Set with all elements inside setA that are not part of setB
+onlyB - a Set with all elements inside setB that are not part of setA
+union - a Set with all elements inside setA and setB
+Example 1
+Input
+setA
+
+=
+Set(4)
+<entries>: (4) [ 1, 2, 3, 4 ]
+size: 4
+<prototype>: Object
+setB
+
+=
+Set(4)
+<entries>: (4) [ 3, 4, 5, 6 ]
+size: 4
+<prototype>: Object
+Output
+{…}
+onlyA: Set(2) {<entries>: Array(2), size: 2 }
+onlyB: Set(2) {<entries>: Array(2), size: 2 }
+union: Set(6) {<entries>: Array(6), size: 6 }
+<prototype>: Object
+ */
+
+function compareSets(setA, setB) {
+  const onlyA = new Set();
+  const onlyB = new Set();
+  const union = new Set();
+
+  for (const value of setA) {
+    if (setB.has(value)) {
+      union.add(value);
+    } else {
+      union.add(value);
+      onlyA.add(value);
+    }
+  }
+
+  for (const value of setB) {
+    if (!setA.has(value)) {
+      onlyB.add(value);
+    }
+    union.add(value);
+  }
+
+  return {
+    onlyA,
+    onlyB,
+    union,
+  };
+}
