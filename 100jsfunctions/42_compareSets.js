2e67f2fe411174ab682511de39b120ee4d0069c0
dmltdev <hone.domitoro@gmail.com>
2024-06-11 12:54:54 +0300

    feat(42): refactor compareSets for clarity

--- 100jsfunctions/42_compareSets.js
+++ 100jsfunctions/42_compareSets.js
@@ -34,29 +34,19 @@ union: Set(6) {<entries>: Array(6), size: 6 }
  */
 
 function compareSets(setA, setB) {
-  const onlyA = new Set();
-  const onlyB = new Set();
-  const union = new Set();
-
-  for (const value of setA) {
-    if (setB.has(value)) {
-      union.add(value);
-    } else {
-      union.add(value);
-      onlyA.add(value);
-    }
-  }
-
-  for (const value of setB) {
-    if (!setA.has(value)) {
-      onlyB.add(value);
-    }
-    union.add(value);
-  }
-
-  return {
-    onlyA,
-    onlyB,
-    union,
+  const solution = {
+    onlyA: new Set(),
+    onlyB: new Set(),
+    union: new Set([...setA, ...setB]),
   };
+
+  for (const value of solution.union) {
+    if (setA.has(value) && !setB.has(value)) {
+      solution.onlyA.add(value);
+    } else if (!setA.has(value)) {
+      solution.onlyB.add(value);
+    }
+  }
+
+  return solution;
 }
