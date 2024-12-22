a10f584675df58486028ae6f80e1bd786e187981
dmltdev <hone.domitoro@gmail.com>
2024-12-22 10:46:53 +0200

    feat(112): hasPathSum

---
+++ leetcoding/easy-problems/easy.112/easy.112.ts
@@ -0,0 +1,81 @@
+/* 
+112. Path Sum
+Easy
+Topics
+Companies
+
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Explanation: The root-to-leaf path with the target sum is shown.
+
+Example 2:
+
+Input: root = [1,2,3], targetSum = 5
+Output: false
+Explanation: There are two root-to-leaf paths in the tree:
+(1 --> 2): The sum is 3.
+(1 --> 3): The sum is 4.
+There is no root-to-leaf path with sum = 5.
+
+Example 3:
+
+Input: root = [], targetSum = 0
+Output: false
+Explanation: Since the tree is empty, there are no root-to-leaf paths.
+
+ 
+
+Constraints:
+
+    The number of nodes in the tree is in the range [0, 5000].
+    -1000 <= Node.val <= 1000
+    -1000 <= targetSum <= 1000
+*/
+
+
+ class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+  if (root === null) {
+    return false;
+  }
+
+  const sum = targetSum - root.val;
+
+  if (root.left === null && root.right === null) {
+    return sum === 0;
+  }
+
+  console.log(root.val, sum);
+
+  return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+}
+
+const tree = new TreeNode(5)
+tree.left = new TreeNode(4);
+tree.right = new TreeNode(8);
+tree.left.left = new TreeNode(11);
+tree.left.left.left = new TreeNode(7);
+tree.left.left.right = new TreeNode(2);
+tree.right.left = new TreeNode(13);
+tree.right.right = new TreeNode(4);
+tree.right.right.right = new TreeNode(1);
+
+console.log(hasPathSum(tree, 22));
