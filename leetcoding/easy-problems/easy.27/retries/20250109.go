36fc169cb8f000ed6642de245acd69632a1d0f7d
dmltdev <hone.domitoro@gmail.com>
2025-01-09 11:30:53 +0200

    feat(27): solve "remove element"

---
+++ leetcoding/easy-problems/easy.27/retries/20250109.go
@@ -0,0 +1,14 @@
+package main
+
+func removeElement(nums []int, val int) int {
+	k := 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[k] = nums[i]
+			k++
+		}
+	}
+
+	return k
+}
