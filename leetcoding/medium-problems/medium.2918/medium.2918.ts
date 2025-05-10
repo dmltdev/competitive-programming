6fd71f53d2e5f40374348f1894d4c399bb24a193
dmltdev <hone.domitoro@gmail.com>
2025-05-10 11:24:35 +0300

    feat(2918): solve "Minimum Equal Sum of Two Arrays After Replacing Zeros"

---
+++ leetcoding/medium-problems/medium.2918/medium.2918.ts
@@ -0,0 +1,60 @@
+/* 
+2918. Minimum Equal Sum of Two Arrays After Replacing Zeros
+Medium
+Topics
+Companies
+Hint
+You are given two arrays nums1 and nums2 consisting of positive integers.
+
+You have to replace all the 0's in both arrays with strictly positive integers such that the sum of elements of both arrays becomes equal.
+
+Return the minimum equal sum you can obtain, or -1 if it is impossible.
+
+ 
+
+Example 1:
+
+Input: nums1 = [3,2,0,1,0], nums2 = [6,5,0]
+Output: 12
+Explanation: We can replace 0's in the following way:
+- Replace the two 0's in nums1 with the values 2 and 4. The resulting array is nums1 = [3,2,2,1,4].
+- Replace the 0 in nums2 with the value 1. The resulting array is nums2 = [6,5,1].
+Both arrays have an equal sum of 12. It can be shown that it is the minimum sum we can obtain.
+Example 2:
+
+Input: nums1 = [2,0,2,0], nums2 = [1,4]
+Output: -1
+Explanation: It is impossible to make the sum of both arrays equal.
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 105
+0 <= nums1[i], nums2[i] <= 106
+*/
+
+function minSum(nums1: number[], nums2: number[]): number {
+  const sum1 = nums1.reduce((acc, curr) => acc + curr, 0);
+  const sum2 = nums2.reduce((acc, curr) => acc + curr, 0);
+  const zeroCount1 = nums1.filter((num) => num === 0).length;
+  const zeroCount2 = nums2.filter((num) => num === 0).length;
+
+  if (zeroCount1 === 0 && zeroCount2 === 0) {
+    return sum1 === sum2 ? sum1 : -1;
+  }
+
+  if (zeroCount1 > 0 && zeroCount2 === 0) {
+    const minSum1 = sum1 + zeroCount1;
+    if (sum2 < minSum1) return -1;
+    return sum2;
+  }
+
+  if (zeroCount2 > 0 && zeroCount1 === 0) {
+    const minSum2 = sum2 + zeroCount2;
+    if (sum1 < minSum2) return -1;
+    return sum1;
+  }
+
+  const minPossible = Math.max(sum1 + zeroCount1, sum2 + zeroCount2);
+  return minPossible;
+}
