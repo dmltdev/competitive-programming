454a2b251d14da6a85004a73f667a454753a110c
dmltdev <hone.domitoro@gmail.com>
2025-12-09 18:06:25 +0200

    feat(383/ts): ransom note - better approach

--- leetcoding/easy-problems/easy.383/easy.383.ts
+++ leetcoding/easy-problems/easy.383/easy.383.ts
@@ -1,23 +1,34 @@
-function canConstruct(ransomNote: string, magazine: string): boolean {
-  const map = new Map<string, number>();
+// Solution 1: Hash Map
+// function canConstruct(ransomNote: string, magazine: string): boolean {
+//   const map = new Map<string, number>();
 
-  for (const c of magazine) {
-    if (map.has(c)) {
-      const v = map.get(c) || 0;
-      map.set(c, v + 1);
-    } else {
-      map.set(c, 1);
-    }
+//   for (const c of magazine) {
+//     if (map.has(c)) {
+//       const v = map.get(c) || 0;
+//       map.set(c, v + 1);
+//     } else {
+//       map.set(c, 1);
+//     }
+//   }
+
+//   for (const c of ransomNote) {
+//     if (map.get(c)) {
+//       const v = map.get(c) || 0;
+//       map.set(c, v - 1);
+//     } else {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+
+// Solution 2: Progressively replace characters from ransomNote by matching them with characters from magazine
+
+function canConstruct(ransomNote: string, magazine: string) {
+  let note: string = ransomNote;
+  for (const char of magazine) {
+    note = note.replace(char, "");
   }
-
-  for (const c of ransomNote) {
-    if (map.get(c)) {
-      const v = map.get(c) || 0;
-      map.set(c, v - 1);
-    } else {
-      return false;
-    }
-  }
-
-  return true;
+  return !note;
 }
