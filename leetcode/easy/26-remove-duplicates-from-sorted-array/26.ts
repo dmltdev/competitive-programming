1b14ec1df547ac8be47b783d16c05807e36c5cd6
dmltdev <hone.domitoro@gmail.com>
2025-12-22 00:29:27 +0200

    feat(26): remove duplicated from sorted array

--- leetcode/easy/26-remove-duplicates-from-sorted-array/26.ts
+++ leetcode/easy/26-remove-duplicates-from-sorted-array/26.ts
@@ -1,8 +1,8 @@
 function removeDuplicates(nums: number[]): number {
-  let k = 0;
+  let k = 1;
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== nums[i - 1]) {
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i - 1] !== nums[i]) {
       nums[k] = nums[i];
       k++;
     }
