d17a1ef3d44d72cd1f7b7a7254c4b3eefded0ecb
dmltdev <hone.domitoro@gmail.com>
2025-12-30 15:58:08 +0200

    chore(229): code style

--- leetcode/medium/229/229.ts
+++ leetcode/medium/229/229.ts
@@ -29,9 +29,12 @@ Follow up: Could you solve the problem in linear time and in O(1) space?
 */
 
 function majorityElement(nums: number[]): number[] {
-  let candidate1 = 0, candidate2 = 1, count1 = 0, count2 = 0; 
+  let candidate1 = 0,
+    candidate2 = 1,
+    count1 = 0,
+    count2 = 0;
 
-  for (let num of nums) {
+  for (const num of nums) {
     if (num === candidate1) {
       count1++;
     } else if (num === candidate2) {
@@ -50,13 +53,14 @@ function majorityElement(nums: number[]): number[] {
     }
   }
 
-  let result:number[] = [];
-  if (nums.filter(n => n === candidate1).length > nums.length / 3) {
-    result.push(candidate1)
+  const result: number[] = [];
+
+  if (nums.filter((n) => n === candidate1).length > nums.length / 3) {
+    result.push(candidate1);
   }
-  if (nums.filter(n => n === candidate2).length > nums.length / 3) {
+  if (nums.filter((n) => n === candidate2).length > nums.length / 3) {
     result.push(candidate2);
   }
 
   return result;
-};
+}
