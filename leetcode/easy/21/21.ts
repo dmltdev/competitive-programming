b545800fa9f53b3ada0364a93be9a17959b5274c
dmltdev <hone.domitoro@gmail.com>
2026-01-17 23:58:34 +0200

    feat(21): merge two sorted lists

--- leetcode/easy/21/21.ts
+++ leetcode/easy/21/21.ts
@@ -36,22 +36,22 @@ class ListNode {
   }
 }
 
-function mergeTwoLists(
-  list1: ListNode | null,
-  list2: ListNode | null
-): ListNode | null {
-  if (!list1) {
-    return list2;
-  }
-  if (!list2) {
-    return list1;
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+  const dummy = new ListNode(0);
+  let current = dummy;
+
+  while (list1 !== null && list2 !== null) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
   }
 
-  if (list1.val < list2.val) {
-    list1.next = mergeTwoLists(list1.next, list2);
-    return list1;
-  } else {
-    list2.next = mergeTwoLists(list1, list2.next);
-    return list2;
-  }
+  current.next = list1 !== null ? list1 : list2;
+
+  return dummy.next;
 }
